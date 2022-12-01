ba766e80779c6808c4e9daefd3ba9185a8bf07b4
deadnxthing <119539361+deadnxthing@users.noreply.github.com>
2022-12-01 03:51:31 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ from apikey import api_key
 import requests
 
 
-name = input('Wprowadź nazwe swojego summonera: ')
+name = input('Wprowadź nazwe swojego przywolywacza: ')
 name = str(name.replace(' ', '%20'))
 
 def kda(name):
@@ -36,49 +36,27 @@ def CS(name):
     match_url = f'https://europe.api.riotgames.com/lol/match/v5/matches/{resp2[0]}'+'?api_key='+api_key
     match_info = requests.get(match_url)
     resp3 = match_info.json()
-    user_index=resp3['metadata']['participants'].index(player_puuid)
-    miniony=resp3['info']['participants'][user_index]['totalMinionsKilled']
-    czas=resp3['info']['participants'][user_index]['timePlayed']
+    index=resp3['metadata']['participants'].index(player_puuid)
+    miniony=resp3['info']['participants'][index]['totalMinionsKilled']
+    czas=resp3['info']['participants'][index]['timePlayed']
     minuty=czas/60
     cs=round(miniony/minuty,1)
     print(cs)
 
 
-#nie dziala huj
 def rank(name):
     url1 = f'https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}'
     name_api_link = url1 +"?api_key="+api_key
     player_info = requests.get(name_api_link)
-    resp1 = player_info.json()  
+    resp1 = player_info.json()
     player_id = resp1['id']
-    url2=f'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/{player_id}'+'?api_key='+api_key
-    info=requests.get(url2)
-    resp=info.json()
-    division=resp['tier']
-    rank=resp['rank']
-    lp=resp['leaguePoints']
-    ranga=f'{division} {rank} {lp} LP '
-    
+    url2 =f'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/{player_id}'+'?api_key='+api_key
+    rank_info=requests.get(url2)
+    rank_info=rank_info.json()
+    ranga=(rank_info[0])
     print(ranga)
 
-
-#rank(name=name)
-
-url1 = f'https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}'
-name_api_link = url1 +"?api_key="+api_key
-player_info = requests.get(name_api_link)
-resp1 = player_info.json()  
-player_id = resp1['id']
-url2=f'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/{player_id}'+'?api_key='+api_key
-info=requests.get(url2)
-resp=info.json()
-division=resp['tier']
-rank=resp['rank']
-lp=resp['leaguePoints']
-ranga=f'{division} {rank} {lp} LP '
-
-print(ranga)
+rank(name=name)
 
 
-
-
+# https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/_pj7SWuX6ln_mAFfdQy8uh9tYRygVilOIHtxWquVDFwxNGqH4fW8fWIW5g?api_key=RGAPI-62ac970f-d313-4e55-a95e-59b6ef103018
